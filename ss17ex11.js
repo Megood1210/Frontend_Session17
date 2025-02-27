4c9f41c550f30ec1327a5194de78d8c960f05c5b
Megood1210 <xuanvinhle06@gmail.com>
2025-02-27 11:54:13 +0700

    Upload bài tập 11

---
+++ ss17ex11.js
@@ -0,0 +1,16 @@
+document.write(Math.abs(-5,5)); // -5,5 là số âm, giá trị tuyệt đối của nó là 5.
+document.write("<br>");
+document.write(Math.abs(10)); // 10 là số dương, giá trị tuyệt đối giữ nguyên của nó là 5.
+document.write("<br>");
+document.write(Math.ceil(4.4)); //4.4 nằm giữa 4 và 5, và vì Math.ceil() làm tròn lên, kết quả là 5.
+document.write("<br>");
+document.write(Math.ceil(5.8)); //4.4 nằm giữa 5 và 6, và vì Math.ceil() làm tròn lên, kết quả là 6.
+document.write("<br>");
+document.write(Math.floor(5.7)); //5.7 nằm giữa 5 và 6, nhưng floor() luôn làm tròn xuống, kết quả là 5.
+document.write("<br>");
+document.write(Math.floor(6.1)); //6.1 nằm giữa 6 và 7, nhưng floor() luôn làm tròn xuống, kết quả là 6.
+document.write("<br>");
+document.write(Math.round(10.1)); //10.1 có phần thập phân 0.1 (nhỏ hơn 0.5), nên làm tròn xuống 10.
+document.write("<br>");
+document.write(Math.round(20.6)); //20.6 có phần thập phân 0.6 (lớn hơn 0.5), nên làm tròn lên 21.
+
